60f828967fb9dc15d91984e0422baee5c668a663
SatyaMac <satyamac@SatyaMacs-MacBook-Pro.local>
2026-01-16 11:03:14 -0500

    feat: migrate to OIDC and 3-tier app deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 # --- 1. PROVIDERS & VPC ---
+data "aws_caller_identity" "current" {}
 terraform {
   required_version = "~> 1.5.0"
   required_providers {
@@ -52,6 +53,50 @@ module "eks" {
 }
 
 # --- 3. CI/CD USER FOR GITHUB ACTIONS ---
+# 1. Create the OIDC Provider (The "Handshake")
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  # This is the standard thumbprint for GitHub's OIDC certificate
+  thumbprint_list = ["1b5113700940728c0b5c1630b427847701e64984"]
+}
+# 2. Create a Role that GitHub can "Assume"
+resource "aws_iam_role" "github_oidc_role" {
+  name = "github-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        # Link directly to the resource we just created
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub": "repo:ligeroweb/k8k8appjan2025:*"
+        }
+      }
+    }]
+  })
+}
+
+# 3. Attach Permissions to this Role
+resource "aws_iam_role_policy_attachment" "github_oidc_ecr" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+# Add EKS access for the role
+resource "aws_iam_role_policy_attachment" "github_oidc_eks" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+output "ROLE_ARN" {
+  value = aws_iam_role.github_oidc_role.arn
+}
 resource "aws_iam_user" "github_actions" {
   name = "github-actions-deployer"
 }
@@ -69,4 +114,7 @@ output "GITHUB_ACCESS_KEY_ID" { value = aws_iam_access_key.github.id }
 output "GITHUB_SECRET_ACCESS_KEY" {
   value     = aws_iam_access_key.github.secret
   sensitive = true
+}
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
